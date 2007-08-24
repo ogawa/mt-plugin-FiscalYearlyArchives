a2bbc8348f7673b657aad9b786d2eb8e9bc4dc7b
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2007-08-24 04:51:52 +0000

    Add FiscalYearlyArchives.
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-FiscalYearlyArchives/trunk@1 b7bee0e5-6241-4529-aa9e-08ae86e46850

---
+++ FiscalYearlyArchives/fiscal_yearly_archives.pl
@@ -0,0 +1,221 @@
+# FiscalYearlyArchives
+#
+# $Id$
+# This software is provided as-is. You may use it for commercial or 
+# personal use. If you distribute it, please keep this notice intact.
+#
+# Copyright (c) 2007 Hirotaka Ogawa
+
+package MT::Plugin::FiscalYearlyArchives;
+use strict;
+use base qw(MT::Plugin);
+
+use MT;
+use MT::Template::Context;
+use MT::Entry;
+use MT::Tag;
+use MT::ObjectTag;
+use MT::Promise qw(force);
+
+our $VERSION = '0.01';
+
+my $plugin = __PACKAGE__->new({
+    name => 'FiscalYearlyArchives',
+    description => 'A plugin for building Fiscal Yearly Archives.',
+    doc_link => 'http://code.as-is.net/wiki/FiscalYearlyArchives',
+    author_name => 'Hirotaka Ogawa',
+    author_link => 'http://profile.typekey.com/ogawa/',
+    version => $VERSION,
+    l10n_class => 'FiscalYearlyArchives::L10N',
+});
+MT->add_plugin($plugin);
+
+use MT::WeblogPublisher;
+sub init_registry {
+    my $plugin = shift;
+    $plugin->registry({
+	tags => {
+	    function => {
+		ArchiveFiscalYear => \&archive_fiscal_year,
+	    },
+	},
+	'archive_types' => {
+	    'FiscalYearly' =>
+		ArchiveType(
+			    name => 'FiscalYearly',
+			    archive_label => \&archive_label,
+			    archive_file => \&archive_file,
+			    archive_title => \&archive_title,
+			    date_range => \&date_range,
+			    archive_group_iter => \&archive_group_iter,
+			    archive_group_entries => \&archive_group_entries,
+			    archive_entries_count => \&archive_entries_count,
+			    dynamic_template => '<$MTArchiveFiscalYear$>',
+			    default_archive_templates => [
+				ArchiveFileTemplate(
+				    label => 'yyyy/index.html',
+				    template => '<$MTArchiveFiscalYear$>/%i',
+				    default => 1
+				),
+			    ],
+			    dynamic_support => 1,
+			    date_based => 1,
+			    template_params => {
+				datebased_only_archive => 1,
+				datebased_fiscal_yearly_archive => 1,
+				module_fiscal_yearly_archives => 1,
+				main_template => 1,
+				archive_template => 1,
+				archive_class => "datebased-fiscal-yearly-archive",
+			    },
+			    ),
+	}
+    });
+}
+
+sub archive_fiscal_year {
+    my ($ctx, %param) = @_;
+    my $ts = $ctx->{current_timestamp};
+    my $tag = $ctx->stash('tag');
+    return $ctx->error(MT->translate("You used an [_1] tag without a date context set up.", "MT$tag"))
+        unless defined $ts;
+    $ts = start_end_fiscal_year($ts);
+    my ($year) = unpack 'A4', $ts;
+    $year;
+}
+
+sub start_end_fiscal_year {
+    my ($ts) = @_;
+    my ($y, $m) = unpack('A4A2', $ts);
+    $y-- if $m < 4;
+    my $start = sprintf "%04d0401000000", $y;
+    return $start unless wantarray;
+    my $end = sprintf "%04d0331235959", $y + 1;
+    ($start, $end);
+}
+
+sub archive_label {
+    $plugin->translate('FISCAL-YEARLY_ADV');
+}
+
+sub archive_file {
+    my ($ctx, %param) = @_;
+    my $timestamp = $param{Timestamp};
+    my $file_tmpl = $param{Template};
+    my $blog = $ctx->{__stash}{blog};
+
+    my $file;
+    if ($file_tmpl) {
+	($ctx->{current_timestamp}, $ctx->{current_timestamp_end}) =
+	    start_end_fiscal_year($timestamp);
+    } else {
+	my $start = start_end_fiscal_year($timestamp);
+	my ($year) = unpack 'A4', $start;
+	$file = sprintf("04d/index", $year);
+    }
+    $file;
+}
+
+sub archive_title {
+    my ($ctx, $entry_or_ts) = @_;
+    my $ts = ref $entry_or_ts ? $entry_or_ts->authored_on : $entry_or_ts;
+    my $start = start_end_fiscal_year($ts);
+    my ($year) = unpack 'A4', $start;
+    my $lang = lc MT->current_language || 'en_us';
+    $lang = 'ja' if lc($lang) eq 'jp';
+    $lang eq 'ja' ? $year . '&#24180;&#24230;' : $year;
+}
+
+sub date_range { start_end_fiscal_year(@_) }
+
+sub archive_group_iter {
+    my ($ctx, $args) = @_;
+    my $blog = $ctx->stash('blog');
+
+    my $sort_order = ($args->{sort_order} || '') eq 'ascend' ? 'ascend' : 'descend';
+    my $order = $sort_order eq 'ascend' ? 'asc' : 'desc';
+
+    require MT::Entry;
+    my $iter = MT::Entry->count_group_by({
+	blog_id => $blog->id,
+	status  => MT::Entry::RELEASE(),
+    }, {
+	## WRONG! WRONG!
+	group => ["extract(year from authored_on)"],
+	$args->{lastn} ? ( limit => $args->{lastn} ) : (),
+	sort => "extract(year from authored_on) $order",
+    })
+	or return $ctx->error("Couldn't get yearly archive list");
+
+    return sub {
+	while (my @row = $iter->()) {
+	    my $date = sprintf("%04d%02d%02d000000", $row[1], 1, 1);
+	    my ($start, $end) = start_end_fiscal_year($date);
+	    return ($row[0], year => $row[1], start => $start, end => $end);
+	}
+	undef;
+    };
+}
+
+sub archive_group_entries {
+    my ($ctx, %param) = @_;
+    my $ts = sprintf("%04d%02d%02d000000", $param{year}, 4, 1)
+	if %param;
+    my ($start, $end);
+    if ($ts) {
+	($start, $end) = start_end_fiscal_year($ts);
+	$ctx->{current_timestamp}     = $start;
+	$ctx->{current_timestamp_end} = $end;
+    } else {
+	$start = $ctx->{current_timestamp};
+	$end   = $ctx->{current_timestamp_end};
+    }
+    my $blog = $ctx->stash('blog');
+    my @entries = MT->model('entry')->load({
+	blog_id     => $blog->id,
+	status      => MT::Entry::RELEASE(),
+	authored_on => [$start, $end],
+    }, {
+	range  => { authored_on => 1 },
+	'sort' => 'authored_on',
+	'direction' => 'descend',
+    })
+	or return $ctx->error("Couldn't get Fiscal-Year archive list");
+    \@entries;
+}
+
+sub archive_entries_count {
+    my ($params) = @_;
+    my $blog     = $params->{Blog};
+    my $at       = $params->{ArchiveType};
+    my $ts       = $params->{Timestamp};
+    my $cat      = $params->{Category};
+    my $auth     = $params->{Author};
+
+    my ($start, $end);
+    if ($ts) {
+	($start, $end) = start_end_fiscal_year($ts);
+    }
+    my $count = MT->model('entry')->count({
+	blog_id => $blog->id,
+	status  => MT::Entry::RELEASE(),
+	( $ts ? ( authored_on => [ $start, $end ] ) : () ),
+	( $auth ? ( author_id => $auth->id ) : () ),
+    }, {
+            ( $ts ? ( range => { authored_on => 1 } ) : () ),
+            (
+                $cat
+                ? (
+                    'join' => [
+                        'MT::Placement', 'entry_id',
+                        { category_id => $cat->id }
+                    ]
+                  )
+                : ()
+            ),
+        }
+    );
+    $count;
+}
+
+1;
---
+++ FiscalYearlyArchives/lib/FiscalYearlyArchives/L10N/en_us.pm
@@ -0,0 +1,12 @@
+# $Id$
+package FiscalYearlyArchives::L10N::ja;
+
+use strict;
+use base 'FiscalYearlyArchives::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+		'FISCAL-YEARLY_ADV' => 'Fiscal Yearly',
+		);
+
+1;
