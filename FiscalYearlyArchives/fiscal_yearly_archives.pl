af994d47d5a01dc230df980d42ef5a5859aeda8e
Hirotaka Ogawa <hirotaka.ogawa@gmail.com>
2007-08-25 19:45:02 +0000

    support Fiscal Start Month configuration.
    git-svn-id: file:///Users/ogawa/svn/mt-plugin-FiscalYearlyArchives/trunk@6 b7bee0e5-6241-4529-aa9e-08ae86e46850

--- FiscalYearlyArchives/fiscal_yearly_archives.pl
+++ FiscalYearlyArchives/fiscal_yearly_archives.pl
@@ -11,15 +11,12 @@ use strict;
 use base qw(MT::Plugin);
 
 use MT;
-use MT::Template::Context;
 use MT::Entry;
-use MT::Tag;
-use MT::ObjectTag;
-use MT::Promise qw(force);
 
 our $VERSION = '0.01';
 
 my $plugin = __PACKAGE__->new({
+    id => 'fiscal_yearly_archives',
     name => 'FiscalYearlyArchives',
     description => q(<MT_TRANS phrase="A plugin for building Fiscal Yearly Archives">),
     doc_link => 'http://code.as-is.net/wiki/FiscalYearlyArchives',
@@ -27,6 +24,11 @@ my $plugin = __PACKAGE__->new({
     author_link => 'http://profile.typekey.com/ogawa/',
     version => $VERSION,
     l10n_class => 'FiscalYearlyArchives::L10N',
+    system_config_template => 'system_config.tmpl',
+    settings => new MT::PluginSettings([
+	['fiscal_start_month', { Default => 4, Scope => 'system' }],
+    ]),
+    
 });
 MT->add_plugin($plugin);
 
@@ -42,56 +44,81 @@ sub init_registry {
 	'archive_types' => {
 	    'FiscalYearly' =>
 		ArchiveType(
-			    name => 'FiscalYearly',
-			    archive_label => \&archive_label,
-			    archive_file => \&archive_file,
-			    archive_title => \&archive_title,
-			    date_range => \&date_range,
-			    archive_group_iter => \&archive_group_iter,
-			    archive_group_entries => \&archive_group_entries,
-			    archive_entries_count => \&archive_entries_count,
-			    default_archive_templates => [
-				ArchiveFileTemplate(
-				    label => 'fiscal/yyyy/index.html',
-				    template => 'fiscal/<$MTArchiveFiscalYear$>/%i',
-				    default => 1
-				),
-			    ],
-			    dynamic_template => 'fiscal/<$MTArchiveFiscalYear$>',
-			    dynamic_support => 1,
-			    date_based => 1,
-			    # ???
-			    template_params => {
-				datebased_only_archive => 1,
-				datebased_fiscal_yearly_archive => 1,
-				module_fiscal_yearly_archives => 1,
-				main_template => 1,
-				archive_template => 1,
-				archive_class => "datebased-fiscal-yearly-archive",
-			    },
-			    ),
+		    name => 'FiscalYearly',
+		    archive_label => \&archive_label,
+		    archive_file => \&archive_file,
+		    archive_title => \&archive_title,
+		    date_range => \&date_range,
+		    archive_group_iter => \&archive_group_iter,
+		    archive_group_entries => \&archive_group_entries,
+		    archive_entries_count => \&archive_entries_count,
+		    default_archive_templates => [
+			ArchiveFileTemplate(
+			    label => 'fiscal/yyyy/index.html',
+			    template => 'fiscal/<$MTArchiveFiscalYear$>/%i',
+			    default => 1
+			),
+		    ],
+		    dynamic_template => 'fiscal/<$MTArchiveFiscalYear$>',
+		    dynamic_support => 1,
+		    date_based => 1,
+		    # ???
+		    template_params => {
+			datebased_only_archive => 1,
+			datebased_fiscal_yearly_archive => 1,
+			module_fiscal_yearly_archives => 1,
+			main_template => 1,
+			archive_template => 1,
+			archive_class => "datebased-fiscal-yearly-archive",
+		    },
+		),
 	}
     });
 }
 
+# This is a dirty hack to omit multiple get_config_value() calls for
+# optimization.  Basically, I don't like the way which a plugin loads
+# and stores its configuration by using MT::PluginSettings.  Because I
+# believe it should be able to get_and_set to the 'registry' in a more
+# flexible and transparent manner.
+our $START_MONTH;
+sub init_request {
+    my $plugin = shift;
+    $plugin->SUPER::init_request(@_);
+    $START_MONTH = $plugin->get_config_value('fiscal_start_month') || 4;
+}
+
+sub ts2fiscal {
+    my ($ts) = @_;
+    my ($y, $m) = unpack('A4A2', $ts);
+    $y-- if $m < $START_MONTH;
+    $y;
+}
+
+# MTArchiveFiscalYear tag
 sub archive_fiscal_year {
     my ($ctx, %param) = @_;
     my $ts = $ctx->{current_timestamp};
     my $tag = $ctx->stash('tag');
     return $ctx->error(MT->translate("You used an [_1] tag without a date context set up.", "MT$tag"))
-        unless defined $ts;
-    $ts = start_end_fiscal_year($ts);
-    my ($year) = unpack 'A4', $ts;
-    $year;
+	unless defined $ts;
+    ts2fiscal($ts);
 }
 
 sub start_end_fiscal_year {
     my ($ts) = @_;
-    my ($y, $m) = unpack('A4A2', $ts);
-    $y-- if $m < 4;
-    my $start = sprintf "%04d0401000000", $y;
+    my $start_year = ts2fiscal($ts);
+    my $start = sprintf("%04d%02d%02d000000", $start_year, $START_MONTH, 1);
     return $start unless wantarray;
-    my $end = sprintf "%04d0331235959", $y + 1;
+
+    my ($end_year, $end_month, $end_day);
+    if ($START_MONTH == 1) {
+	($end_year, $end_month, $end_day) = ($start_year, 12, 31);
+    } else {
+	($end_year, $end_month) = ($start_year + 1, $START_MONTH - 1);
+	$end_day = MT::Util::days_in($end_month, $end_year);
+    }
+    my $end = sprintf("%04d%02d%02d235959", $end_year, $end_month, $end_day);
     ($start, $end);
 }
 
@@ -110,8 +137,7 @@ sub archive_file {
 	($ctx->{current_timestamp}, $ctx->{current_timestamp_end}) =
 	    start_end_fiscal_year($timestamp);
     } else {
-	my $start = start_end_fiscal_year($timestamp);
-	my ($year) = unpack 'A4', $start;
+	my $year = ts2fiscal($timestamp);
 	$file = sprintf("04d/index", $year);
     }
     $file;
@@ -120,8 +146,7 @@ sub archive_file {
 sub archive_title {
     my ($ctx, $entry_or_ts) = @_;
     my $ts = ref $entry_or_ts ? $entry_or_ts->authored_on : $entry_or_ts;
-    my $start = start_end_fiscal_year($ts);
-    my ($year) = unpack 'A4', $start;
+    my $year = ts2fiscal($ts);
     my $lang = lc MT->current_language || 'en_us';
     $lang = 'ja' if lc($lang) eq 'jp';
     $lang eq 'ja' ? $year . '&#24180;&#24230;' : $year;
@@ -151,7 +176,7 @@ sub archive_group_iter {
     while (my @row = $iter->()) {
 	my $ts = sprintf("%04d%02d%02d000000", $row[1], $row[2], 1);
 	my ($start, $end) = start_end_fiscal_year($ts);
-	my ($year) = unpack 'A4', $start;
+	my $year = ts2fiscal($ts);
 	if ($year == $prev_year) {
 	    $count_groups[-1]->{count} += $row[0];
 	} else {
@@ -177,7 +202,7 @@ sub archive_group_iter {
 
 sub archive_group_entries {
     my ($ctx, %param) = @_;
-    my $ts = sprintf("%04d%02d%02d000000", $param{fiscal_year}, 4, 1)
+    my $ts = sprintf("%04d%02d%02d000000", $param{fiscal_year}, $START_MONTH, 1)
 	if %param;
     my ($start, $end);
     if ($ts) {
@@ -217,11 +242,11 @@ sub archive_entries_count {
     my $count = MT->model('entry')->count({
 	blog_id => $blog->id,
 	status  => MT::Entry::RELEASE(),
-	($ts ? (authored_on => [$start, $end]) : ()),
-	($auth ? (author_id => $auth->id) : ()),
+	$ts   ? (authored_on => [$start, $end]) : (),
+	$auth ? (author_id => $auth->id) : (),
     }, {
-	($ts ? (range => { authored_on => 1 }) : ()),
-	($cat ? ('join' => ['MT::Placement', 'entry_id', { category_id => $cat->id }]) : ()),
+	$ts   ? (range => { authored_on => 1 }) : (),
+	$cat  ? ('join' => ['MT::Placement', 'entry_id', { category_id => $cat->id }]) : (),
     });
     $count;
 }
